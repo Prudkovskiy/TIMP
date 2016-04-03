a8cfdc145bed8d281a040c0c15a4841cbc93cf33
Prudkovskiy <nickolay.prud@gmail.com>
2016-04-03 23:07:48 +0300

    hi

---
+++ lab2/TAMP_lab2.cpp
@@ -0,0 +1,37 @@
+// TIIMP Laba2.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char  s[255];
+	char str[255];
+	char* result;
+
+	cout << "File name? "; cin >> s;
+	ifstream fin(s);
+	while (!fin.eof()) {
+		fin.getline(s, 255);	//считываем из файла строку в s	
+		if ((strstr(s, "\\func")) != NULL) { //проверка на наличие в s "\func"
+			result = strstr(s, "\\func"); //result указывает на то место в s, где стоит \func
+			result += 6;//result теперь указывает на 6 байт правее (перепрыгнули "\func ")
+			if (strstr(result, "*/") != NULL) //проверяем, заканчивается ли данная строка  на "*/"
+			{
+				strcpy(str, result); //копируем result  в str 
+				str[strlen(result) - 2] = '\0';// "затираем" */
+				cout << str << endl;
+			}
+			else
+				cout << result << endl;
+		}
+	}
+	system("pause");
+
+	return 0;
+}
+
